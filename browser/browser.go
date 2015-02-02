f9ff78bbb782c1c6a16ae87fffe7eb90b25eb85c
Felipe Coury <felipe.coury@gmail.com>
2015-02-02 14:35:05 -0200

    Retrieves WorkItems correctly

--- browser/browser.go
+++ browser/browser.go
@@ -2,44 +2,67 @@ package browser
 
 import (
 	"bytes"
+	"fmt"
 	"net/http"
 )
 
 type Browser struct {
 	Cookies      []*http.Cookie
 	LastResponse http.Response
+	Debug        bool
 }
 
 func NewBrowser() *Browser {
-	return &Browser{}
+	return &Browser{Debug: true}
 }
 
-func (b *Browser) Request(method string, url string, data string) {
-	var dataBuffer *Buffer
-
-	if data != "" {
-		dataBuffer = bytes.NewBufferString(data)
-
-	}
+func (b *Browser) Request(method string, url string, data string) (*http.Response, error) {
+	dataBuffer := bytes.NewBufferString(data)
 
 	r, err := http.NewRequest(method, url, dataBuffer)
-	for i, c := range Cookies {
+	if err != nil {
+		return nil, err
+	}
+
+	r.Header.Add("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8")
+
+	for _, c := range b.Cookies {
 		r.AddCookie(c)
 	}
 
 	if err != nil {
-		return r, err
+		return nil, err
+	}
+
+	if b.Debug {
+		b.ShowCookies()
+		fmt.Printf("Requesting: %s\n", url)
 	}
 
 	resp, err := http.DefaultTransport.RoundTrip(r)
+	if err != nil {
+		return resp, err
+	}
+
+	if b.Debug {
+		fmt.Printf("Status: %s\nHeaders: %s\n\n", resp.Status, resp.Header)
+	}
 
 	if err != nil {
-		return r, err
+		return resp, err
 	}
 
-	for i, c = range resp.Cookies {
-		append(b.Cookies, c)
+	for _, c := range resp.Cookies() {
+		b.Cookies = append(b.Cookies, c)
 	}
 
-	return r, err
+	return resp, err
+}
+
+func (b *Browser) ShowCookies() {
+	fmt.Println("\n\n-------- [COOKIES]")
+	for i, c := range b.Cookies {
+		fmt.Printf(" Cookie %d - %s\n", i, c.Name)
+	}
+	fmt.Println("-------- [COOKIES]\n\n")
 }
---
+++ models/models.go
@@ -0,0 +1,58 @@
+package models
+
+import "encoding/xml"
+
+type Envelope struct {
+	Body Body `xml:"Body"`
+}
+
+type Body struct {
+	Response Response `xml:"response"`
+}
+
+type Response struct {
+	Method      string      `xml:"method"`
+	Interface   string      `xml:"interface"`
+	ReturnValue ReturnValue `xml:"returnValue"`
+}
+
+type ReturnValue struct {
+	Type  string `xml:"type"`
+	Value Value  `xml:"value"`
+}
+
+type Value struct {
+	StartIndex     int64 `xml:"startIndex"`
+	TotalCount     int64 `xml:"totalCount"`
+	EstimatedTotal int64 `xml:"estimatedTotal"`
+	Limit          int64 `xml:"limit"`
+	Token          string
+	Headers        []Header `xml:"headers"`
+	Rows           []Row    `xml:"rows"`
+}
+
+type Header struct {
+	AttributeId   string `xml:"attributeId"`
+	AttributeType string `xml:"attributeType"`
+	Label         string `xml:"label"`
+	IsOrderable   string `xml:"isOrderable"`
+	Alignment     string `xml:"alignment"`
+	Width         string `xml:"width"`
+	IconOnly      string `xml:"iconOnly"`
+	LabelOnly     string `xml:"labelOnly"`
+}
+
+type Row struct {
+	Id          int64    `xml:"id"`
+	ItemId      string   `xml:"itemId"`
+	StateGroup  int64    `xml:"stateGroup"`
+	Labels      []string `xml:"labels"`
+	LocationUri string   `xml:"locationUri"`
+}
+
+func NewFromXml(xmlData []byte) (Envelope, error) {
+	var b Envelope
+	err := xml.Unmarshal(xmlData, &b)
+
+	return b, err
+}
